141c0385d37c171a1f9b7621394a3aaab1e76ee6
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-07-18 07:08:51 +0300

    chore: add global exception filter and configure dynamic CORS origin from .env

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,60 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const req = ctx.getRequest<Request>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: string | string[] = 'Internal server error';
+
+    // NestJS
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const response = exception.getResponse();
+      message =
+        typeof response === 'string'
+          ? response
+          : (response as { message: string | string[] }).message;
+    }
+
+    // Mongoose validation
+    else if (exception instanceof mongoose.Error.ValidationError) {
+      status = HttpStatus.BAD_REQUEST;
+      message = Object.values(exception.errors).map((err) => err.message);
+    }
+
+    // Duplicate key
+    else if (
+      typeof exception === 'object' &&
+      exception !== null &&
+      'code' in exception &&
+      exception.code === 11000 &&
+      'keyValue' in exception
+    ) {
+      status = HttpStatus.CONFLICT;
+
+      const keyValue = (exception as { keyValue: Record<string, any> })
+        .keyValue;
+      const field = Object.keys(keyValue)[0];
+      message = `This ${field} is already used.`;
+    }
+
+    res.status(status).json({
+      statusCode: status,
+      message,
+      path: req.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { json } from 'express';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const env = configService.get<string>('NODE_ENV');
+  const origin =
+    env === 'production'
+      ? configService.get<string>('CORS_ORIGINS') || '*'
+      : '*';
+
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.use(helmet());
   app.use(json({ limit: '2mb' }));
   app.enableCors({
-    origin: '*',
+    origin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
