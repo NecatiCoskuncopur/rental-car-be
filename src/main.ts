873ec98aba14604af142e22d6f977c7e73cd2dcf
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-08-29 18:51:13 +0300

    chore: setup Swagger with initial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { json } from 'express';
+import * as fs from 'fs';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
@@ -28,6 +30,29 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Rental Car API')
+    .setDescription('API documentation for the Rental Car system')
+    .setVersion('1.0.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customCss: `
+    .swagger-ui .opblock .opblock-summary-description {
+      background-color:#e74c3c;
+      color: white;
+      padding: 4px 6px;
+      font-weight: semi-bold;
+      border-radius: 4px;
+      margin-left: 24px;
+    }
+    `,
+  });
+
+  const document = documentFactory();
+  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document, null, 2));
 
   await app.listen(Number(process.env.PORT));
 }
