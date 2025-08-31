a8d7afe7bb3888513c1ae6df32a3f9657a3a9495
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-08-31 13:57:38 +0300

    chore: add MongoDB connection setup to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ThrottlerModule } from '@nestjs/throttler';
+import mongoose from 'mongoose';
 
 import { envSchema } from './common/validations/env.validation';
 import { HealthController } from './health/health.controller';
@@ -20,6 +22,22 @@ import { UserModule } from './user/user.module';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       validationSchema: envSchema,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        connectionFactory: (connection: mongoose.Connection) => {
+          if ((connection.readyState as number) === 1) {
+            console.log('DB Connected');
+          } else {
+            console.warn('DB Connection Failed');
+          }
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
   ],
